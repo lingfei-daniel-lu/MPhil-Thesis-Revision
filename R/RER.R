5f094f4a4e89361f2a3a5421cc0cb6532c63be63
GassoLu <lulingfei0409@gmail.com>
2023-02-20 18:09:12 +0800

    2.20 update

--- R/RER.R
+++ R/RER.R
@@ -16,7 +16,8 @@ ggplot()+
   geom_line(data=ER_USD,aes(x=Year, y=RER, linetype = "RER"),size=1)+
   xlab("Year")+
   ylab("Exchange Rates of USD")+
-  theme_bw() + theme(panel.grid=element_blank(),legend.title=element_blank())
+  theme_bw() + theme(panel.grid=element_blank(),legend.title=element_blank())+
+  ggtitle("China's Annual Exchange Rate with the U.S. (Base Value in 1999 = 100)")
 ggsave("USD.eps", width = 6.5, height = 3.0)
 
 ER_EUR <- subset(ER,ER$Currency=="Euro",select = c(Year,NER,RER))
@@ -28,5 +29,6 @@ ggplot()+
   geom_line(data=ER_EUR,aes(x=Year, y=RER, linetype = "RER"),size=1)+
   xlab("Year")+
   ylab("Exchange Rates of EUR")+
-  theme_bw() + theme(panel.grid=element_blank(),legend.title=element_blank())
+  theme_bw() + theme(panel.grid=element_blank(),legend.title=element_blank())+
+  ggtitle("China's Annual Exchange Rate with Eurozone (Base Value in 1999 = 100)")
 ggsave("EUR.eps", width = 6.5, height = 3.0)
